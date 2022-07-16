2b9adb2a2fa89ae888a8403547ddb9652e2d9364
woojeong kong <coolstevevc@gmail.com>
2022-07-16 17:50:36 +0900

    feat: Add lint, gitignore setting

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "eslint-config-prettier",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  ignorePatterns: [".eslintrc.js"],
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "prettier/prettier": "error",
+    indent: ["warn", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["warn", "double"],
+    semi: ["warn", "always"],
+    "no-var": "error",
+    "no-unused-vars": "warn",
+  }
+};
