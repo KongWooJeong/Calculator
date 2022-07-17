c0f17a163db2a3b19e33ef558b4683714e093ced
woojeong kong <coolstevevc@gmail.com>
2022-07-18 06:57:10 +0900

    refactor: Update constructor pattern

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,3 @@
-interface CalculatorInfo {
-  previousOperand: string;
-  currentOperand: string;
-  operation: string;
-  displayedResult: string;
-}
-
 const $calculatorResult: HTMLInputElement | null =
   document.querySelector(".calculator-result");
 
@@ -13,14 +6,136 @@ const $numberContainer: HTMLDivElement | null =
 const $signContainer: HTMLDivElement | null =
   document.querySelector(".sign-container");
 
-const calculatorInfo: CalculatorInfo = {
-  previousOperand: "",
-  currentOperand: "",
-  operation: "",
-  displayedResult: "",
-};
+class Calculator {
+  calculatorResultElement: HTMLInputElement | null;
+  currentOperand: string;
+  previousOperand: string;
+  operator: string;
+  displayedResult: string;
 
-let isSign = true;
+  constructor(calculatorResultElement: HTMLInputElement | null) {
+    this.calculatorResultElement = calculatorResultElement;
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operator = "";
+    this.displayedResult = "0";
+  }
+
+  clear() {
+    this.currentOperand = "";
+    this.previousOperand = "";
+    this.operator = "";
+    this.displayedResult = "0";
+  }
+
+  setPercent() {
+    if (this.currentOperand === "") {
+      return;
+    }
+
+    this.operator = "/";
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "100";
+
+    this.caculate();
+  }
+
+  changePlusMinusSign() {
+    if (this.currentOperand === "" && this.displayedResult !== "0") {
+      if (this.displayedResult[0] === "-") {
+        this.displayedResult = this.displayedResult.slice(1);
+      } else {
+        this.displayedResult = "-" + this.displayedResult;
+      }
+
+      this.previousOperand = this.displayedResult;
+
+      return;
+    } else if (this.currentOperand === "" || this.currentOperand === "0") {
+      return;
+    }
+
+    if (this.currentOperand[0] === "-") {
+      this.currentOperand = this.currentOperand.slice(1);
+    } else {
+      this.currentOperand = "-" + this.currentOperand;
+    }
+
+    this.displayedResult = this.currentOperand;
+  }
+
+  selectNubmer(number: string) {
+    if (number === "." && this.currentOperand.includes(".")) {
+      return;
+    }
+
+    if (this.currentOperand === "0" && number === "0") {
+      return;
+    }
+
+    if (
+      (this.currentOperand === "" || this.currentOperand === "0") &&
+      number === "."
+    ) {
+      this.currentOperand = "0";
+    } else if (this.currentOperand === "0") {
+      this.currentOperand = "";
+    }
+
+    this.currentOperand += number;
+    this.displayedResult = this.currentOperand;
+  }
+
+  selectOperator(operator: string) {
+    if (this.currentOperand === "") {
+      return;
+    }
+
+    if (this.previousOperand !== "") {
+      this.caculate();
+    }
+
+    this.operator = operator;
+    this.previousOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
+
+  caculate() {
+    let caculationResult: number;
+    const previousNumber: number = parseFloat(this.previousOperand);
+    const currentNumber: number = parseFloat(this.currentOperand);
+
+    switch (this.operator) {
+      case "+":
+        caculationResult = previousNumber + currentNumber;
+        break;
+      case "-":
+        caculationResult = previousNumber - currentNumber;
+        break;
+      case "*":
+        caculationResult = previousNumber * currentNumber;
+        break;
+      case "/":
+        caculationResult = previousNumber / currentNumber;
+        break;
+      default:
+        return;
+    }
+
+    this.currentOperand = caculationResult.toString();
+    this.displayedResult = this.currentOperand;
+    this.operator = "";
+    this.previousOperand = "";
+  }
+
+  updateDisplay() {
+    if (this.calculatorResultElement) {
+      this.calculatorResultElement.innerText = this.displayedResult;
+    }
+  }
+}
+
+const calculator = new Calculator($calculatorResult);
 
 $numberContainer?.addEventListener("click", ({ target }: MouseEvent) => {
   const $button: HTMLButtonElement = target as HTMLButtonElement;
@@ -29,9 +144,8 @@ $numberContainer?.addEventListener("click", ({ target }: MouseEvent) => {
     return;
   }
 
-  calculatorInfo.currentOperand += $button.innerText;
-  calculatorInfo.displayedResult = calculatorInfo.currentOperand;
-  updateDisplay(calculatorInfo);
+  calculator.selectNubmer($button.innerText);
+  calculator.updateDisplay();
 });
 
 $signContainer?.addEventListener("click", ({ target }: MouseEvent) => {
@@ -40,97 +154,18 @@ $signContainer?.addEventListener("click", ({ target }: MouseEvent) => {
   if ($button.className === "sign-container") {
     return;
   }
-  const selectedOperation = $button.innerText;
-  const { currentOperand } = calculatorInfo;
 
-  if (selectedOperation === "=") {
-    calculatorInfo.currentOperand = calculate(calculatorInfo);
-    calculatorInfo.previousOperand = currentOperand;
-    calculatorInfo.operation = "";
-  } else if (selectedOperation === "%") {
-    calculatorInfo.operation = selectedOperation;
-    calculatorInfo.previousOperand = currentOperand;
-    calculatorInfo.currentOperand = calculate(calculatorInfo);
-  } else if (selectedOperation === "c") {
-    calculatorInfo.currentOperand = "";
-    calculatorInfo.previousOperand = "";
-    calculatorInfo.displayedResult = "";
-    calculatorInfo.operation = "";
-  } else if (selectedOperation === "+/-") {
-    calculatorInfo.currentOperand = isSign
-      ? "-" + currentOperand
-      : currentOperand.slice(1);
-    calculatorInfo.displayedResult = isSign
-      ? "-" + currentOperand
-      : currentOperand.slice(1);
-    isSign = !isSign;
+  if ($button.className === "sign-equal") {
+    calculator.caculate();
+  } else if ($button.className === "sign-clear") {
+    calculator.clear();
+  } else if ($button.className === "sign-plus-minus") {
+    calculator.changePlusMinusSign();
+  } else if ($button.className === "sign-percent") {
+    calculator.setPercent();
   } else {
-    selectOperation(calculatorInfo, $button.innerText);
+    calculator.selectOperator($button.innerText);
   }
 
-  updateDisplay(calculatorInfo);
+  calculator.updateDisplay();
 });
-
-// module
-
-function selectOperation(
-  calculatorInfo: CalculatorInfo,
-  selectedOperation: string
-) {
-  const { currentOperand, previousOperand } = calculatorInfo;
-
-  if (currentOperand === "") {
-    return;
-  }
-
-  if (previousOperand !== "") {
-    calculate(calculatorInfo);
-  }
-
-  calculatorInfo.operation = selectedOperation;
-  calculatorInfo.previousOperand = currentOperand;
-  calculatorInfo.currentOperand = "";
-}
-
-function updateDisplay(calculatorInfo: CalculatorInfo) {
-  const { displayedResult } = calculatorInfo;
-
-  if ($calculatorResult) {
-    $calculatorResult.value = displayedResult;
-  }
-}
-
-function calculate(calculatorInfo: CalculatorInfo): string {
-  let result: number | null = null;
-
-  const { operation, previousOperand, currentOperand } = calculatorInfo;
-
-  switch (operation) {
-    case "+":
-      result = parseFloat(previousOperand) + parseFloat(currentOperand);
-      break;
-    case "-":
-      result = parseFloat(previousOperand) - parseFloat(currentOperand);
-      break;
-    case "*":
-      result = parseFloat(previousOperand) * parseFloat(currentOperand);
-      break;
-    case "/":
-      result = parseFloat(previousOperand) / parseFloat(currentOperand);
-      break;
-    case "%":
-      result = parseFloat(previousOperand) / 100;
-      break;
-    default:
-      break;
-  }
-
-  if (result) {
-    calculatorInfo.currentOperand = result.toString();
-    calculatorInfo.displayedResult = result.toString();
-    calculatorInfo.previousOperand = "";
-    return result.toString();
-  }
-
-  return "";
-}
